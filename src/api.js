34d68d687f7c1d5ec7dca0f3da85029127bb0691
Amaan Ullah <theamaan13@gmail.com>
2023-11-24 00:42:12 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "942c257190msh43f29c8edb72f2ep1c9598jsnff7456733a2c",// enter your rapid api key here
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "5557244f2d31d0e782345cc3f2e9a05b";
